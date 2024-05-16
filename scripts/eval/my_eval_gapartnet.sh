47674f8bda8530679e1487869764ba4b7677bd90
Qile.Xu <xql438814395@gmail.com>
2024-05-16 01:41:46 +0000

    modify test script

--- scripts/eval/my_eval_gapartnet.sh
+++ scripts/eval/my_eval_gapartnet.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-MODEL_VERSION=ShapeLLM_7B
+MODEL_VERSION=ShapeLLM_13B
 TAG=gapartnet_v1.0
 
 mkdir -p ./playground/data/eval/gapartnet/results
 
 python -m llava.eval.eval_gapartnet \
-    --answers-file ./playground/data/eval/gapartnet/answers/$MODEL_VERSION'_'$TAG'_0000'.jsonl \
+    --answers-file ./playground/data/eval/gapartnet/answers/$MODEL_VERSION'_'$TAG.jsonl \
     --gt-file ./playground/data/eval/gapartnet/gt.jsonl \
     --output-file ./playground/data/eval/gapartnet/results/$MODEL_VERSION'_'$TAG.jsonl \
     --max_workers 16
